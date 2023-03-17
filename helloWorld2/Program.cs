4db4e6f672698b9e1a17e291201a17292e1814ff
luisjaarcoding@gmail.com <luisjaarcoding@gmail.com>
2023-03-17 10:41:02 -0500

    Practicing with models

---
+++ helloWorld2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using helloWorld2.Models;
+
+namespace helloWorld2
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Instance of Model
+            Computer myComputer = new Computer() {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "RTX 2060"
+            };
+
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.HasWifi);
+            Console.WriteLine(myComputer.HasLTE);
+            Console.WriteLine(myComputer.VideoCard);
+        }
+    }
+}
